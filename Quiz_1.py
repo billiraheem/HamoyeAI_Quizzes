7f31568489e9fd5ecfdf5f3f6c165c6d0c06fe26
billiraheem <Babdulraheem@cleanmeter.io>
2024-04-15 17:16:36 +0100

    quiz 1

---
+++ Quiz_1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+file = "C:\Users\User\Documents\GitHub\HamoyeAI_Quizzes\FoodBalanceSheets_E_Africa_NOFLAG.csv"
+
+df = pd.read_csv(file, encoding='latin-1')
